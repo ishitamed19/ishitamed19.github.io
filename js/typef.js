907272ec86f1149ba143f2829b574c90f11f4ce2
Ishita Mediratta <ishitamediratta19@gmail.com>
2017-01-24 11:54:51 +0530

    Create typef.js

---
+++ js/typef.js
@@ -0,0 +1,19 @@
+ $(function(){
+        $("#typed").typed({
+            // strings: ["Typed.js is a <strong>jQuery</strong> plugin.", "It <em>types</em> out sentences.", "And then deletes them.", "Try it out!"],
+            stringsElement: $('#typed-strings'),
+            typeSpeed: 40,
+            backDelay: 500,
+            loop: true,
+            contentType: 'html', // or text
+            // defaults to false for infinite loop
+            loopCount: false,
+            callback: function(){ foo(); },
+            resetCallback: function() { newTyped(); }
+        });
+        $(".reset").click(function(){
+            $("#typed").typed('reset');
+        });
+    });
+    function newTyped(){ /* A new typed object */ }
+    function foo(){ console.log("Callback"); }
